33649021ed072fbe97ebced1fa684f50c7f9939c
Emil Pettersson <epette17@student.aau.dk>
2019-03-22 10:46:07 +0100

    Initial unity commit

---
+++ Assets/SerialPortTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class SerialPortTest : MonoBehaviour
+{
+    SerialPort stream = new SerialPort("COM5", 9600);
+
+    int indexArd = 0;
+
+    void Start()
+    {
+        stream.Open();
+    }
+
+    void Update()
+    {
+
+        string value = stream.ReadLine();
+        indexArd = int.Parse(value);
+        Debug.Log(indexArd);
+        
+    }
+
+    void OnGUI()
+    {
+        string newString = "Connected: " + indexArd;
+        GUI.Label(new Rect(10, 10, 100, 50), newString);
+
+    }
+
+}
