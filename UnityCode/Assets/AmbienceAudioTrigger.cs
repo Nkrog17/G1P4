f9a518dd75b9828936e00638b94ee40c11dac7da
DanielHendriksen <daynee_93@hotmail.com>
2019-04-16 13:31:53 +0200

    In progress lyd

---
+++ UnityCode/Assets/AmbienceAudioTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbienceAudioTrigger : MonoBehaviour
+{
+    public float num;
+    public GameObject AmbienceTrigger;
+    public AudioSource ambience;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter()
+    {
+        
+        
+     
+        
+        ambience.Play();
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
