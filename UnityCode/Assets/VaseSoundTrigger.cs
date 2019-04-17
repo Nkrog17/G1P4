9ce934109744477e316441c5c33416fcffe7d657
Niklasskjold <35644289+Niklasskjold@users.noreply.github.com>
2019-04-17 14:04:38 +0200

    vase virker

--- UnityCode/Assets/VaseSoundTrigger.cs
+++ UnityCode/Assets/VaseSoundTrigger.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class VaseSoundTrigger : MonoBehaviour
 {
+    public GameObject vaseNotBroken;
+    public GameObject vaseBroken;
+
     public AudioSource vase;
     bool havePlayed = false;
 
     private void OnTriggerEnter(Collider collider)
     {
+        
+
         if (!havePlayed)
         {
             vase = GetComponent<AudioSource>();
             vase.Play();
             havePlayed = true;
+            vaseNotBroken.SetActive(false);
+
+            vaseBroken.SetActive(true);
+            
+            
+
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-       
+        vaseBroken.SetActive(false);
+        
+
+        
 
     }
 
