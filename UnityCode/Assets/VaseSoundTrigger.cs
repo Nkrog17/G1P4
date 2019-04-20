a3856a3fd56d62fd1aaee9e3c67e124448b5d248
Nkrog17 <nkrog17@student.aau.dk>
2019-04-20 17:12:10 +0200

    Added creepy vase-light script

--- UnityCode/Assets/VaseSoundTrigger.cs
+++ UnityCode/Assets/VaseSoundTrigger.cs
@@ -10,6 +10,16 @@ public class VaseSoundTrigger : MonoBehaviour
     public AudioSource vase;
     bool havePlayed = false;
 
+    public GameObject Lamp1;
+    public GameObject Lamp2;
+    public GameObject Lamp3;
+    public GameObject Lamp4;
+    public GameObject Lamp5;
+    public GameObject Lamp6;
+    public GameObject Lamp7;
+
+    public GameObject VaseLamp;
+
     private void OnTriggerEnter(Collider collider)
     {
         
@@ -24,7 +34,9 @@ public class VaseSoundTrigger : MonoBehaviour
 
             vaseNotBroken.SetActive(false);
             vaseBroken.SetActive(true);
-            
+            StartCoroutine(waiter());
+
+
             
 
         }
@@ -34,8 +46,25 @@ public class VaseSoundTrigger : MonoBehaviour
     {
         vaseBroken.SetActive(false);
         
+    }
 
-        
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(1);
+        Lamp1.GetComponent<LightController>().lightSwitch();
+        yield return new WaitForSeconds(1);
+        Lamp2.GetComponent<LightController>().lightSwitch();
+        yield return new WaitForSeconds(1);
+        Lamp3.GetComponent<LightController>().lightSwitch();
+        VaseLamp.GetComponent<LightController>().lightSwitch();
+        yield return new WaitForSeconds(1);
+        Lamp4.GetComponent<LightController>().lightSwitch();
+        yield return new WaitForSeconds(1);
+        Lamp5.GetComponent<LightController>().lightSwitch();
+        yield return new WaitForSeconds(1);
+        Lamp6.GetComponent<LightController>().lightSwitch();
+        yield return new WaitForSeconds(1);
+        Lamp7.GetComponent<LightController>().lightSwitch();
 
     }
 
