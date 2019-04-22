2c6409a82e6fc19ca2bcf496dd24d981e74e829a
DanielHendriksen <daynee_93@hotmail.com>
2019-04-22 14:43:03 +0200

    added logfile

---
+++ UnityCode/Assets/Logfile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Logfile : MonoBehaviour
+
+
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WriteToFile("you suck!!!");
+    }
+
+    
+    public void WriteToFile(string message)
+    {
+        using (System.IO.StreamWriter logFile = new System.IO.StreamWriter(@"LogFile.txt"))
+        {
+            logFile.WriteLine(message);
+        }
+           
+        
+    }
+}
