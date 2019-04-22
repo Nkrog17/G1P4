97589a0c833ead063da0426a45308fd50628fe65
Emil Pettersson <epette17@student.aau.dk>
2019-04-22 15:56:39 +0200

    Crying sound, plays all around

---
+++ UnityCode/Assets/CryingSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CryingSound : MonoBehaviour
+{
+    
+    public GameObject door;
+    public AudioSource crying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        crying = door.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        crying.Play();
+
+
+    }
+}
