3838a2a7e2490602c90e94220e3198d142bcd08c
Niklasskjold <35644289+Niklasskjold@users.noreply.github.com>
2019-04-25 12:24:34 +0200

    hearbeat

--- UnityCode/Assets/Heartbeat.cs
+++ UnityCode/Assets/Heartbeat.cs
@@ -1,21 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Heartbeat : MonoBehaviour
 {
-    public AudioSource heartbeat;
+    public AudioSource heartBeat;
+   public float speedRegulator;
+    bool playHeartBeat;
+    SerialPortScript heartRate;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-       
+        //brug waitforseconds
+        
+        heartBeat.PlayDelayed(speedRegulator);
+        heartBeat.loop = true;
+
+        //while (playHeartBeat)
+       // {
+            
+            //heartBeat.PlayDelayed(speedRegulator);
+
+        //
+
+        //if (heartRate.latestHR > 80)
+        //{
+           // playHeartBeat = true;
+        //}
+        
+
+
+       // while (playHeartBeat) {
+          //  speedRegulator = heartRate.latestHR;
+        //}
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        heartbeat.PlayDelayed(1);
-        heartbeat.loop = true;
+        
+        
         
     }
 }
