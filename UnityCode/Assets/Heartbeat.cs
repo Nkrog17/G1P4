12481223fd919914b2099d02583d0bc8bfe7eb07
Niklasskjold <35644289+Niklasskjold@users.noreply.github.com>
2019-04-25 11:19:29 +0200

    hejsa

---
+++ UnityCode/Assets/Heartbeat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heartbeat : MonoBehaviour
+{
+    public AudioSource heartbeat;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        heartbeat.PlayDelayed(1);
+        heartbeat.loop = true;
+        
+    }
+}
