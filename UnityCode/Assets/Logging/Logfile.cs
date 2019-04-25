f95b17e5ba4ab65ce47a4e0f0bbf007811a85e62
Emil Pettersson <epette17@student.aau.dk>
2019-04-25 12:26:17 +0200

    logging

---
+++ UnityCode/Assets/Logging/Logfile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Logfile : MonoBehaviour
+   {
+    public GameObject serialport;
+    int SPSHR;
+    int SPSGSR;
+    float timer = 0.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {   
+        //SPSHR = serialport.GetComponent<SerialPortScript>().latestHR;
+       // SPSGSR = serialport.GetComponent<SerialPortScript>().latestGSR;
+        //WriteToFile("HR="+SPSHR+" GSR=" + SPSGSR + " (X,Y,Z)=" + GameObject.Find("FPSController").transform.position + " Time=" + timer);
+    }
+    private void Update()
+    {
+        SPSHR = serialport.GetComponent<SerialPortScript>().latestHR;
+        SPSGSR = serialport.GetComponent<SerialPortScript>().latestGSR;
+        timer += Time.deltaTime;
+        
+        WriteToFile("HR=" + SPSHR + " GSR=" + SPSGSR + " (X,Y,Z)=" + GameObject.Find("FPSController").transform.position + " Time=" + timer);
+        Debug.Log("HR=" + SPSHR + " GSR=" + SPSGSR + " (X,Y,Z)=" + GameObject.Find("FPSController").transform.position + " Time=" + timer);
+        
+    }
+
+    public void WriteToFile(string message)
+    {
+        using (System.IO.StreamWriter logFile = new System.IO.StreamWriter(@"Assets/Logging/LogFile.txt", true))
+        {
+            logFile.WriteLine(message);
+        }
+           
+        
+    }
+}
