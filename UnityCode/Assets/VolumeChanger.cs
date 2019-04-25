f769b2acec538602fc6b78e5a1c6a9beffe8f11a
Nkrog17 <nkrog17@student.aau.dk>
2019-04-25 16:07:34 +0200

    Changed collision boxes

--- UnityCode/Assets/VolumeChanger.cs
+++ UnityCode/Assets/VolumeChanger.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//This script sets the volume on all audio to something lower than 1.
 public class VolumeChanger : MonoBehaviour
 {
     public float volume;
