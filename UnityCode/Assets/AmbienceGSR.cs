3519ede855aaf98b044b3cc42da113669dba5aac
Emil Pettersson <epette17@student.aau.dk>
2019-04-29 11:57:59 +0200

    d

--- UnityCode/Assets/AmbienceGSR.cs
+++ UnityCode/Assets/AmbienceGSR.cs
@@ -8,27 +8,27 @@ public class AmbienceGSR : MonoBehaviour
     public AudioSource ambience;
     public GameObject serialPort;
     float gsr;
-    float avgblgsr;
+    float avgBlGsr;
     // Start is called before the first frame update
     void Start()
     {
-        ambience.volume = 0.5f;
+       
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        avgblgsr = serialPort.GetComponent<SerialPortScript>().avgGSRbaseLine;
+        avgBlGsr = serialPort.GetComponent<SerialPortScript>().avgGSRbaseLine;
         gsr = serialPort.GetComponent<SerialPortScript>().latestGSR;
 
         if (serialPort.GetComponent<SerialPortScript>().GSRspikeNow)
         {
-            ambience.volume = 1 - (avgblgsr/100*gsr)+0.25f;
+            ambience.volume = 1 - ((avgBlGsr/100)*gsr)+0.25f;
         }
         else
         {
-            ambience.volume = 0.5f;
+            ambience.volume = 0.25f;
         }
 
         if (ambience.volume > 1)
--- UnityCode/Assets/SerialPortScript.cs
+++ UnityCode/Assets/SerialPortScript.cs
@@ -24,7 +24,7 @@ public class SerialPortScript : MonoBehaviour
     int nonsenseHR = 5;
     bool baseLine = true;
 
-    public static bool biofeedback = false;
+    public static bool biofeedback = true;
 
     public int latestHR;
     public int latestHRLog;
