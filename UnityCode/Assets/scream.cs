98acb923e71e19f3252a0af2aa54b5322397fa57
Niklasskjold <35644289+Niklasskjold@users.noreply.github.com>
2019-04-30 12:35:32 +0200

    scream

---
+++ UnityCode/Assets/scream.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scream : MonoBehaviour
+{
+    public AudioSource screamSound;
+    bool havePlayed = false;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+
+
+        if (!havePlayed)
+        {
+            screamSound = GetComponent<AudioSource>();
+            screamSound.Play();
+            havePlayed = true;
+
+
+
+
+
+
+
+
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+    }
+}
