f05501432dd9e0d6715e7608efbd149bd2a05d24
Emil Pettersson <epette17@student.aau.dk>
2019-04-30 11:34:07 +0200

    animation door 7 redone

---
+++ UnityCode/Assets/Animations/doorClose2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorClose2 : MonoBehaviour
+{
+    public GameObject door;
+    Animator anim;
+    public AudioSource knirk;
+    bool knirkHavePlayed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        knirk = door.GetComponent<AudioSource>();
+        anim = door.GetComponent<Animator>();
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (ColDoorSlam.door4Open == true)
+        {
+            anim.SetTrigger("doorClose2");
+
+            if (!knirkHavePlayed)
+            {
+                knirk.PlayDelayed(0.4f);
+                knirkHavePlayed = true;
+            }
+        }
+    }
+}
