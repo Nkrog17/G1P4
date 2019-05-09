7ab0c4d8051177941cb7fb6c1c4e547c743f410a
Nkrog17 <nkrog17@student.aau.dk>
2019-05-09 13:34:17 +0200

    Quick fix

--- UnityCode/Assets/SerialPortScript.cs
+++ UnityCode/Assets/SerialPortScript.cs
@@ -8,7 +8,7 @@ public class SerialPortScript : MonoBehaviour
 {
     Thread myThread; 
 
-    SerialPort stream = new SerialPort("COM4", 9600);
+    SerialPort stream = new SerialPort("COM6", 9600);
 
     List<int> allHR = new List<int>();
     List<int> allGSR = new List<int>();
@@ -25,7 +25,7 @@ public class SerialPortScript : MonoBehaviour
     int nonsenseGSR = 5;
     int nonsenseHR = 5;
     public static bool baseLine = true;
-    public static bool biofeedback = false;
+    public static bool biofeedback = true;
 
     bool stopAvgGSR;
     public int latestHR;
